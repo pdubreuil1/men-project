f0741f51358344089d70e58a4244a1daff86f4d7
pdubreuil1 <pierre.dubreuil@telecom-paristech.fr>
2017-03-23 00:31:45 +0100

    parallel tournament

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-from tournament import seq_tournament
+from tournament import seq_tournament, parallel_tournament
 
 import numpy as np
 
 N = 20
 q = 0.2
+k = 3
 
-histogram_of_winners = np.zeros(N)
-for run in range(100000):
-    winner = seq_tournament(N, q, strength_distrib='uniform')
+histogram_of_winners = np.zeros(2**k)
+for run in range(1000):
+    winner = parallel_tournament(k, q, strength_distrib='uniform')
     histogram_of_winners[winner] += 1
-print histogram_of_winners
+print(histogram_of_winners)
--- tournament.py
+++ tournament.py
@@ -22,13 +22,13 @@ def seq_tournament(N, q, strength_distrib='uniform'):
         id_player_1, id_player_2 = np.random.choice(list_rank_players,
                                                     2,
                                                     replace=False)
-        if id_player_1 > id_player_2:
+        if id_player_1 < id_player_2:
             id_player_weak = id_player_2
             id_player_strong = id_player_1
         else:
             id_player_weak = id_player_1
             id_player_strong = id_player_2
-        if np.random.binomial(1, 1-q) == 1:
+        if np.random.binomial(1, q) == 1:
             # weaker player stays
             list_rank_players.remove(id_player_strong)
         else:
@@ -37,3 +37,42 @@ def seq_tournament(N, q, strength_distrib='uniform'):
 
         round_id += 1
     return list_rank_players[0]
+
+
+def parallel_tournament(k, q, strength_distrib):
+    '''
+    Args:
+        - k (integer): power of 2
+        - q (float in [0:1]): upset probability
+        - strength_distrib (str): distribution used to select strength of
+                                  each player
+    Output:
+        - winner_rank : the rank of the winner.
+    '''
+    N = 2**k
+    if strength_distrib == 'uniform':
+        strength_players = np.random.uniform(low=0.0, high=1.0, size=N)
+    rank_players = strength_players.argsort()[::-1]
+    list_rank_players = list(rank_players)
+    round_id = 0
+    N1 = N
+    while round_id < k-1:
+        list_permuted = np.random.permutation(list_rank_players)
+        players_1 = list_permuted[:int((N1)/2)]
+        players_2 = list_permuted[int((N1)/2):N]
+        for i in range(len(players_1)):
+            if players_1[i] < players_2[i]:
+                id_player_weak = players_2[i]
+                id_player_strong = players_1[i]
+            else:
+                id_player_weak = players_1[i]
+                id_player_strong = players_2[i]
+            if np.random.binomial(1, q) == 1:
+                # weaker player stays
+                list_rank_players.remove(id_player_strong)
+            else:
+                # stronger player stays
+                list_rank_players.remove(id_player_weak)
+        round_id += 1
+        N1 = N1 / 2
+    return list_rank_players

5a47f3584b3fb502e480e8c84ab1334b1d53ce8b
zaccharieramzi <zaccharie.ramzi@gmail.com>
2017-03-23 13:11:08 +0100

    change variable names, added some pep8, corrected docs

--- tournament.py
+++ tournament.py
@@ -1,12 +1,11 @@
 import numpy as np
-from random import randint
 
 
 def seq_tournament(N, q, strength_distrib='uniform'):
     '''
     Args:
         - N (integer): number of players
-        - q (float in [0:1]): upset probability
+        - q (float in [0:1/2]): upset probability
         - strength_distrib (str): distribution used to select strength of
                                   each player
     Output:
@@ -16,26 +15,25 @@ def seq_tournament(N, q, strength_distrib='uniform'):
         strength_players = np.random.uniform(low=0.0, high=1.0, size=N)
     rank_players = strength_players.argsort()[::-1]
     list_rank_players = list(rank_players)
-    round_id = 0
-    while round_id < N-1:
+    for round_id in range(N - 1):
         # select two players
-        id_player_1, id_player_2 = np.random.choice(list_rank_players,
-                                                    2,
-                                                    replace=False)
-        if id_player_1 < id_player_2:
-            id_player_weak = id_player_2
-            id_player_strong = id_player_1
+        rank_player_1, rank_player_2 = np.random.choice(
+            list_rank_players,
+            2,
+            replace=False)
+        if rank_player_1 < rank_player_2:
+            rank_player_weak = rank_player_2
+            rank_player_strong = rank_player_1
         else:
-            id_player_weak = id_player_1
-            id_player_strong = id_player_2
+            rank_player_weak = rank_player_1
+            rank_player_strong = rank_player_2
         if np.random.binomial(1, q) == 1:
             # weaker player stays
-            list_rank_players.remove(id_player_strong)
+            list_rank_players.remove(rank_player_strong)
         else:
             # stronger player stays
-            list_rank_players.remove(id_player_weak)
+            list_rank_players.remove(rank_player_weak)
 
-        round_id += 1
     return list_rank_players[0]
 
 
@@ -43,7 +41,7 @@ def parallel_tournament(k, q, strength_distrib):
     '''
     Args:
         - k (integer): power of 2
-        - q (float in [0:1]): upset probability
+        - q (float in [0:1/2]): upset probability
         - strength_distrib (str): distribution used to select strength of
                                   each player
     Output:

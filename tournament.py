337212335e51b8b32a5c5abc9952d39611ca581a
zaccharieramzi <zaccharie.ramzi@gmail.com>
2017-03-23 13:19:44 +0100

    change variable names and way of doing loops, added some pep8

--- tournament.py
+++ tournament.py
@@ -52,25 +52,24 @@ def parallel_tournament(k, q, strength_distrib):
         strength_players = np.random.uniform(low=0.0, high=1.0, size=N)
     rank_players = strength_players.argsort()[::-1]
     list_rank_players = list(rank_players)
-    round_id = 0
-    N1 = N
-    while round_id < k-1:
+    for round_id in range(k - 1):
         list_permuted = np.random.permutation(list_rank_players)
-        players_1 = list_permuted[:int((N1)/2)]
-        players_2 = list_permuted[int((N1)/2):N]
-        for i in range(len(players_1)):
-            if players_1[i] < players_2[i]:
-                id_player_weak = players_2[i]
-                id_player_strong = players_1[i]
+        rank_players_1 = list_permuted[:N // 2]
+        rank_players_2 = list_permuted[N // 2:N]
+        for rank_player_1, rank_player_2 in zip(
+            rank_players_1, rank_players_2
+        ):
+            if rank_player_1 < rank_player_2:
+                rank_player_weak = rank_player_2
+                rank_player_strong = rank_player_1
             else:
-                id_player_weak = players_1[i]
-                id_player_strong = players_2[i]
+                rank_player_weak = rank_player_1
+                rank_player_strong = rank_player_2
             if np.random.binomial(1, q) == 1:
                 # weaker player stays
-                list_rank_players.remove(id_player_strong)
+                list_rank_players.remove(rank_player_strong)
             else:
                 # stronger player stays
-                list_rank_players.remove(id_player_weak)
-        round_id += 1
-        N1 = N1 / 2
-    return list_rank_players
+                list_rank_players.remove(rank_player_weak)
+        N = len(list_rank_players)
+    return list_rank_players[0]
